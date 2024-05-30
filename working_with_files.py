cbdcccbbc0778c95c66e188f8c2794a4c4eea321
Дмитрий <dimakol2005@mail.ru>
2024-05-30 18:01:38 +0500

    add check exist of dynamic directory func

--- working_with_files.py
+++ working_with_files.py
@@ -6,3 +6,7 @@ def clear_directory(direct_path: str = 'images/dynamic'):
     shutil.rmtree(direct_path)
     os.mkdir(direct_path)
 
+
+def check_directory(direct_path: str = 'images/dynamic'):
+    if not os.path.isdir(direct_path):
+        os.mkdir(direct_path)
